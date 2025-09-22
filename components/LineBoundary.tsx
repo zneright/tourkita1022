1d7ffd31e2a3a7140606cdcc82b0f53aaf035ce5
Joseph Ryan Chico <chico.213413@caloocan.sti.edu.ph>
2025-09-22 20:52:41 +0800

    updates adjusted

--- components/LineBoundary.tsx
+++ components/LineBoundary.tsx
@@ -1,8 +1,8 @@
 import boundaries from '../data/boundaries.json'
-import palacio from '../data/palacio.json'
+import trademarks from '../data/trademarks.json'
 import { ShapeSource, LineLayer, FillLayer, FillExtrusionLayer } from '@rnmapbox/maps'
-import mapua from '../data/mapua.json'
-import gs from '../data/guadalupe_shrine.json'
+
+
 import { Button, View } from 'react-native'
 const LineBoundary = ({ visible }: { visible: boolean }) => {
    
@@ -21,47 +21,20 @@ const LineBoundary = ({ visible }: { visible: boolean }) => {
                   
                 />
             </ShapeSource>
-
-            
-            <ShapeSource id="palacioSource" shape={palacio}>
-                <FillExtrusionLayer
-                    id="palacioExtrusion"
-                    style={{
-                        fillExtrusionColor: "#4F46E5",   // building color
-                        fillExtrusionHeight: 40,         // height in meters
-                        fillExtrusionBase: 0,            // base height (ground level)
-                        fillExtrusionOpacity: 0.8,     
-                        visibility: visible ? "visible" : "none",  // slightly transparent
-                    }}
-                />
-            </ShapeSource>
-
-            <ShapeSource id="mapuaSource" shape={mapua}>
+            <ShapeSource id = "trademarksSource" shape ={trademarks}>
                 <FillExtrusionLayer
                     id="mapuaExtrusion"
                     style={{
                         fillExtrusionColor: "#4F46E5",   // building color
-                        fillExtrusionHeight: 39     ,         // height in meters
+                        fillExtrusionHeight: 39,         // height in meters
                         fillExtrusionBase: 0,            // base height (ground level)
-                        fillExtrusionOpacity: 0.8,  
+                        fillExtrusionOpacity: 0.8,
                         visibility: visible ? "visible" : "none",     // slightly transparent
                     }}
                 />
             </ShapeSource>
-
-
-            <ShapeSource id="gsSource" shape={gs}>
-                <FillExtrusionLayer
-                    id="gsExtrusion"
-                    style={{
-                        fillExtrusionColor: "#4F46E5",   // building color
-                        fillExtrusionHeight: 10,         // height in meters
-                        fillExtrusionBase: 0,            // base height (ground level)
-                        fillExtrusionOpacity: 0.8,   
-                        visibility: visible ? "visible" : "none",    // slightly transparent
-                    }}
-                />
-            </ShapeSource>
+            
+            
             <View style={{ position: 'absolute', top: 50, right: 20 }}>
                 <Button
                     title={visible ? "Hide Buildings" : "Show Buildings"}
